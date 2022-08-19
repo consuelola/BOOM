3aa35cf8ebec33bb72596de613d7267bd2de2f19
marineLM <marine-le-morvan@hotmail.fr>
2022-08-19 11:32:49 +0200

    Update machine learning

---
+++ Machine_learning/src/run_experiments.py
@@ -0,0 +1,218 @@
+import pandas as pd
+import numpy as np
+import requests
+from itertools import product
+from io import StringIO
+from sklearn.base import clone
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import cross_validate, GroupShuffleSplit,\
+    StratifiedGroupKFold
+from sklearn.inspection import permutation_importance
+import matplotlib.pyplot as plt
+import missingno as msno
+from utils import preprocessing
+from utils import GridSearchCV_with_groups
+from utils import plot_scatterplots
+from utils import plot_confusion_matrix
+from utils import get_design_matrix
+from utils import get_models
+
+# Choose the data to be used for learning.
+data_type = 'majors_only'
+# data_type = 'traces_only'
+# data_type = 'majors_and_traces'
+
+# Download the dataset from the ESPRI server as .csv
+url = "https://data.ipsl.fr/repository/TephraDatabase/TephraDataBase.csv"
+s = requests.get(url).text
+df = pd.read_csv(StringIO(s), encoding="latin1", low_memory=False)
+
+# in case the ESPRI server is down:
+# df = pd.read_csv(
+# "..assets/Data/BOOMDataset.csv", encoding = 'UTF-8', low_memory =False )
+
+# Data preprocessing. For more info on the preprocessing of data, the reader is
+# referred to the BOOMDataset_preprocessing notebook
+df = preprocessing(df)
+
+# Filter samples for which volcano ID is unknown.
+df_volcanoes = df.loc[df.Volcano != 'Unknown'].copy()
+
+# X_volcanoes only contains the columns related to the desired chemical
+# elements
+# df_volcanoes and X_volcanoes are restricted to the rows for which the
+# desired chemical elements are not all missing values.
+df_volcanoes, X_volcanoes = get_design_matrix(
+    df_volcanoes, data_type
+)
+
+# Define target attributes (volcano) and list of SampleIDs to consider in the
+# train/test splits.
+df_volcanoes['Volcano'] = df_volcanoes['Volcano'].astype("category")
+df_volcanoes['SampleID'] = df_volcanoes['SampleID'].astype("category")
+# convert stringgs to interger codes
+y = np.array(df_volcanoes['Volcano'].cat.codes, dtype='int8')
+SampleID = np.array(df_volcanoes['SampleID'].cat.codes)
+# Keep track of code-strings correspondance
+volcano_list = np.array(df_volcanoes['Volcano'].cat.categories)
+SampleID_list = np.array(df_volcanoes['SampleID'].cat.categories)
+
+# A few sanity checks
+# -------------------
+unique, counts = np.unique(SampleID, return_counts=True)
+n_sampleID = len(counts)
+print(f'There are {n_sampleID} unique sampleIDs:')
+
+# Print number of sample observations per class
+unique, counts = np.unique(y, return_counts=True)
+n_classes = len(counts)
+print(f'There are {n_classes} volcans distributed as follows:')
+for u, c in zip(unique, counts):
+    print(f'id: {u}, volcán: \
+          \033[1m{volcano_list[u]}\033[0m, sample observations: {c}'
+          )
+
+# Visualize the missing data
+# msno.matrix(X_volcanoes)
+# plt.show()
+
+print('Percentage of missing values for each elements')
+X_volcanoes.isna().mean().round(4) * 100
+
+# Define the models to be trained
+# -------------------------------
+imputations, models, grids = get_models()
+
+imputation_names = list(imputations.keys())
+model_names = list(models.keys())
+if 'traces' in data_type:
+    model_names.remove('gb')
+    model_components = list(product(imputation_names, model_names))
+    model_components.append((None, 'gb'))  # gb does not require imputation
+else:
+    # If only majors are used, there are very few missing values so a simple
+    # imputation will be used.
+    model_names.remove('gb')
+    model_components = [('mean_imp', model_name) for model_name in model_names]
+    model_components.append((None, 'gb'))  # gb does not require imputation
+
+
+def get_clf_grid(comps):
+    imp, mod = comps
+    if imp is not None:
+        clf = make_pipeline(
+            clone(imputations[imp]), StandardScaler(), clone(models[mod])
+        )
+        clf_name = '_'.join(comps)
+    else:
+        clf = make_pipeline(StandardScaler(), clone(models[mod]))
+        clf_name = mod
+    grid = grids[mod]
+
+    return clf, grid, clf_name
+
+
+# Run cross-validation with grid search for all models.
+results = {}
+for comps in model_components:
+    print(comps)
+
+    clf, grid, clf_name = get_clf_grid(comps)
+
+    est = GridSearchCV_with_groups(
+        clf, grid, cv_test_size=0.2, cv_n_splits=5, n_jobs=5)
+
+    gss = GroupShuffleSplit(test_size=0.2, n_splits=5, random_state=0)
+    # gss = StratifiedGroupKFold(n_splits=5, random_state=0)
+
+    cv = cross_validate(est,
+                        X_volcanoes,
+                        y,
+                        groups=SampleID,
+                        scoring=['accuracy', 'balanced_accuracy'],
+                        fit_params={'groups': SampleID},
+                        cv=gss,
+                        return_estimator=True,
+                        n_jobs=1)
+
+    results[clf_name] = cv
+
+# Save results
+model_names = list(results.keys())
+measures = ['fit_time', 'score_time', 'test_accuracy',
+            'test_balanced_accuracy']
+for measure in measures:
+    res = pd.DataFrame([results[c][measure] for c in model_names])
+    res = res.transpose()
+    res.columns = model_names
+    res.to_csv(f'../results/{data_type}/{measure}.csv')
+
+
+# Visualize results for the first fold
+for comps in model_components:
+    clf, grid, clf_name = get_clf_grid(comps)
+
+    # recover the first outer cross-validation split
+    train_out, test_out = next(
+        gss.split(X_volcanoes, groups=SampleID)
+    )
+    # recover the fitted predictor for this split
+    est = results[clf_name]['estimator'][0].best_estimator_
+
+    X_test_out = X_volcanoes.iloc[test_out]
+    y_test_out = y[test_out]
+    pred = est.predict(X_test_out)
+    volcano_list = df_volcanoes.Volcano.cat.categories
+
+    # plot imputation and classification in different scatter plots in order to
+    # have an idea of which samples are badly classified
+    plot_scatterplots(X_test_out, y_test_out,
+                      'SiO2_normalized', 'K2O_normalized',
+                      est, pred, volcano_list, name=f'{data_type}/{clf_name}',
+                      save=True)
+    if 'traces' in data_type:
+        plot_scatterplots(X_test_out, y_test_out, 'SiO2_normalized', 'La',
+                          est, pred, volcano_list,
+                          name=f'{data_type}/{clf_name}', save=True)
+        plot_scatterplots(X_test_out, y_test_out, 'Rb', 'Sr',
+                          est, pred, volcano_list,
+                          name=f'{data_type}/{clf_name}', save=True)
+        plot_scatterplots(X_test_out, y_test_out, 'Cs', 'La',
+                          est, pred, volcano_list,
+                          name=f'{data_type}/{clf_name}', save=True)
+
+    # plot the confusion matrix
+    volcanoes_by_latitude = np.asarray(
+        ['Llaima', 'Sollipulli', 'Caburga-Huelemolle', 'Villarrica',
+         'Quetrupillán', 'Lanín', 'Huanquihue Group', 'Mocho-Choshuenco',
+         'Carrán-Los Venados', 'Puyehue-Cordón Caulle',
+         'Antillanca-Casablanca', 'Osorno', 'Calbuco', 'Yate', 'Apagado',
+         'Hornopirén', 'Huequi', 'Michinmahuida', 'Chaitén', 'Corcovado',
+         'Yanteles', 'Melimoyu', 'Mentolat', 'Cay'
+         'Macá', 'Hudson', 'Lautaro', 'Viedma', 'Aguilera', 'Reclus',
+         'Monte Burney'])
+
+    y_test_names = volcano_list[y_test_out]
+    y_pred_names = volcano_list[pred]
+    plot_confusion_matrix(
+        y_test_names, y_pred_names, labels=volcanoes_by_latitude,
+        name=f'{data_type}/ConfusionMatrix_{clf_name}', save=True
+    )
+
+    # plot permutation importance
+    result = permutation_importance(
+        est, X_test_out, y_test_out, n_repeats=10, random_state=42, n_jobs=3
+    )
+    sorted_idx = result.importances_mean.argsort()
+
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.boxplot(result.importances[sorted_idx].T, vert=False,
+               labels=X_test_out.columns[sorted_idx]
+               )
+    ax.set_title("Permutation Importances (test set)")
+    fig.tight_layout()
+    plt.savefig(
+        f'../figures/{data_type}/permutation_importance_{clf_name}.png',
+        dpi=300, bbox_inches='tight')
+    plt.close()
---
+++ Machine_learning/src/utils.py
@@ -0,0 +1,429 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from itertools import product
+from sklearn.model_selection import GroupShuffleSplit
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+from sklearn.base import BaseEstimator, ClassifierMixin, clone
+from joblib import Parallel, delayed
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer, SimpleImputer, KNNImputer
+from sklearn.linear_model import LogisticRegression, BayesianRidge
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor,\
+     HistGradientBoostingClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def preprocessing(df):
+
+    # 1. First of all, we drop rows corresponding to samples not analyzed for
+    # geochemistry, as well as outliers, samples for which the volcanic source
+    # is uncertain, and samples with Analytical Totals lower than 94 wt.%, 
+    # as they might correspond to altered samples.
+
+    is_register = df.TypeOfRegister.isin(
+        ['Pyroclastic material', 'Effusive material'])
+    isnot_outlier = df.Flag.str.contains(
+        'Outlier', na=False, case=False) == False
+    isnot_VolcanicSourceIssue = df.Flag.str.contains(
+        'VolcanicSource_Issue', na=False, case=False) == False
+    df.SiO2 = df.SiO2.replace(np.nan, -1)
+    isnot_altered = ((df.Total > 95) & (df.SiO2 != -1)) | (df.SiO2 == -1)
+    df.SiO2 = df.SiO2.replace(-1, np.nan)
+    df = df.loc[
+        is_register & isnot_outlier & isnot_VolcanicSourceIssue & isnot_altered
+        ]
+    n, _ = df.shape
+    # print(f'There are {n} rows left.')
+
+    # 2. In second place, we will replace some of the values in the Dataset.
+    # 2.1 Replace element concentrations registered as "0" with
+    # "below detection limit" (bdl).
+    # Because a value equal to zero is not possible to determine with the
+    # current analytical techniques, thus bdl is more accurate.
+
+    for elemento in ["SiO2", "TiO2", "Al2O3", "FeO", "Fe2O3",
+                     "MnO", "MgO", "CaO", "Na2O", "K2O", "P2O5",
+                     "Cl", 'Rb', 'Sr', 'Y', 'Zr', 'Nb',
+                     'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd',
+                     'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho',
+                     'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta',
+                     'Pb', 'Th', 'U']:
+        df[elemento] = df[elemento].replace(to_replace=0, value='bdl')
+
+    # 2.2 Replace the various missing values placeholders by np.nan
+    df.replace(to_replace='n.a.', value=np.nan, inplace=True)
+    df.replace(to_replace='Not analyzed', value=np.nan, inplace=True)
+    df.replace(to_replace='-', value=np.nan, inplace=True)
+    df.replace(to_replace='Not determined', value=np.nan, inplace=True)
+    df.replace(to_replace='n.d', value=np.nan, inplace=True)
+    df.replace(to_replace='n.d.', value=np.nan, inplace=True)
+    df.replace(to_replace='<0.01', value=np.nan, inplace=True)
+    df.replace(to_replace='<0.1', value=np.nan, inplace=True)
+    df.replace(to_replace='<1', value=np.nan, inplace=True)
+    df.replace(to_replace='<5', value=np.nan, inplace=True)
+    df.replace(to_replace='<6', value=np.nan, inplace=True)
+    df.replace(to_replace='<10', value=np.nan, inplace=True)
+    df.replace(to_replace='Over range', value=np.nan, inplace=True)
+    df.replace(to_replace='bdl', value=np.nan, inplace=True)
+
+    # 2.3 Make sure major and trace elements correspond to numbers and not
+    # strings.
+    df.loc[:, 'Rb':'U'] = df.loc[:, 'Rb':'U'].astype('float')
+    df.loc[:, 'SiO2_normalized':'K2O_normalized'] = df.loc[
+        :, 'SiO2_normalized':'K2O_normalized'].astype('float')
+
+    # 3. Because Fe can be analyzed in different states
+    # (FeO, Fe2O3, FeOT, Fe2O3T), the columns describing Fe have many missing
+    # values
+    # but which can be filled by transforming one form of Fe into another.
+    # Because most of the samples in the BOOM dataset have been
+    # analyzed by Electron Microscopy which analyzes Fe as FeOT,
+    # we calculate FeOT for all the samples and drop the other rows
+    # (Fe2O3, Fe2O3T, FeO) as they are redundant.
+
+    # case 1: Fe is presented as Fe2O3 and FeO in the original publication
+    ind = (~df.SiO2_normalized.isna() &
+           df.FeOT_normalized.isna() &
+           ~df.FeO_normalized.isna() &
+           ~df.Fe2O3_normalized.isna() &
+           df.Fe2O3T_normalized.isna()
+           )
+    df.loc[ind, 'FeOT_normalized'] = (
+        df.FeO_normalized.loc[ind]+df.Fe2O3_normalized.loc[ind]*0.899)
+
+    # case 2: Fe is presented as Fe2O3T in the original publication
+    ind = (~df.SiO2_normalized.isna() &
+           df.FeOT_normalized.isna() &
+           df.FeO_normalized.isna() &
+           ~df.Fe2O3T_normalized.isna() &
+           df.Fe2O3_normalized.isna()
+           )
+
+    df.loc[ind, 'FeOT_normalized'] = df.Fe2O3T_normalized.loc[ind]*0.899
+
+    df.drop(['FeO_normalized', 'Fe2O3_normalized', 'Fe2O3T_normalized'],
+            axis=1, inplace=True)
+
+    # 4. When training the models, all sample observations corresponding to the
+    # same sample should either be in the train or test sets.
+    # Thus, we will check if there is any volcanic center with information from
+    # only one sample ID.
+    co = pd.crosstab(df.Volcano, df.SampleID)
+    _, n_sampleID = co.shape
+    # print(f'There are {n_sampleID} unique samples IDs')
+    is_nonzero = co > 0
+    n_volcan_per_sampleID = is_nonzero.sum(axis=0)
+    unique, counts = np.unique(n_volcan_per_sampleID, return_counts=True)
+    ind_ids = np.where(is_nonzero.sum(axis=0) == 2)[0]
+    # print(f'There are {len(ind_ids)} sampleIDs which contain several
+    # observations from several volcanoes:')
+    # print([co.columns[ind_ids].values[i] for i in range(len(ind_ids))])
+
+    n_sampleID_per_volcan = is_nonzero.sum(axis=1)
+    ind_ids = np.where(n_sampleID_per_volcan == 1)[0]
+    # print(f'There is {len(ind_ids)} volcanic center whose observations all
+    # come from the same sample IDs:')
+    # print([co.index[i] for i in ind_ids])
+
+    df = df[df.Volcano != co.index[ind_ids[0]]]
+
+    # print(f'There are {len(df)} observations left.')
+
+    # 5. We will drop the volcanoes with less than 10 observations.
+    Cay = df.Volcano == 'Cay'
+    CordonC = df.Volcano == 'Cordón Cabrera'
+    Corcovado = df.Volcano == 'Corcovado'
+    Yanteles = df.Volcano == 'Yanteles'
+
+    df = df.loc[~Cay & ~CordonC & ~Corcovado & ~Yanteles]
+    n, p = df.shape
+    # print(f'The dataset now has {n} samples.')
+    return df
+
+
+class GridSearchCV_with_groups(BaseEstimator, ClassifierMixin):
+
+    def __init__(self, estimator, param_grid, cv_test_size, cv_n_splits,
+                 n_jobs=None):
+        self.estimator = estimator
+        self.param_grid = param_grid
+        self.cv_test_size = cv_test_size
+        self.cv_n_splits = cv_n_splits
+        self.n_jobs = n_jobs
+
+    def fit(self, X, y, groups):
+
+        if isinstance(X, pd.DataFrame):
+            X = X.to_numpy()
+        if isinstance(y, pd.core.series.Series):
+            y = y.to_numpy()
+
+        keys = self.param_grid.keys()
+        values = self.param_grid.values()
+        self.params_ = [
+            dict(zip(keys, combination)) for combination in product(*values)]
+
+        gss = GroupShuffleSplit(
+            test_size=self.cv_test_size, n_splits=self.cv_n_splits)
+
+        out = Parallel(n_jobs=self.n_jobs)(
+            delayed(self.fit_one)(
+                X[train], y[train], X[test], y[test], comb, id_split, id_comb)
+            for id_comb, comb in enumerate(self.params_)
+            for id_split, (train, test) in enumerate(
+                gss.split(X, groups=groups)
+                )
+            )
+
+        self.scores_ = np.empty((self.cv_n_splits, len(self.params_)))
+        for id_split, id_comb, score in out:
+            self.scores_[id_split, id_comb] = score
+
+        median_score = np.median(self.scores_, axis=0)
+        best_comb = np.argmax(median_score)
+
+        self.best_params_ = self.params_[best_comb]
+        self.best_score_ = median_score[best_comb]
+
+        # Refit on the whole dataset with the best paraps
+        self.best_estimator_ = clone(
+            self.estimator.set_params(**self.best_params_))
+
+        self.best_estimator_.fit(X, y)
+
+    def predict(self, X):
+        return self.best_estimator_.predict(X)
+
+    def score(self, X, y):
+        return self.best_estimator_.score(X, y)
+
+    def fit_one(self, X_train_in, y_train_in, X_test_in, y_test_in, comb,
+                id_split, id_comb):
+        est = clone(self.estimator.set_params(**comb))
+        est.fit(X_train_in, y_train_in)
+        return id_split, id_comb, est.score(X_test_in, y_test_in)
+
+
+def plot_scatterplots(X_test_out, y_test_out, chem_A, chem_B, est, pred,
+                      volcano_list, name='default', target_type='volcano',
+                      save=False):
+
+    ind_wrong = pred != y_test_out
+    if len(est.steps) == 3:
+        X_test_imp = Pipeline(est.steps[:-2]).fit_transform(
+            X_test_out)
+    else:
+        # case of Hist Gradient Boosting for which there is no imputation
+        # We choose to impute by the mean by default.
+        imp = SimpleImputer()
+        X_test_imp = imp.fit_transform(X_test_out)
+    X_test_imp = pd.DataFrame(X_test_imp, columns=X_test_out.columns)
+    y_test_names = volcano_list[y_test_out]
+
+    # Plot Original data (how are missing values treated?)
+    # Only fully observed points plotted?
+    fig, axes = plt.subplots(1, 3, figsize=(15, 4), sharex=True, sharey=True)
+    sns.scatterplot(
+        x=X_test_out.loc[:, chem_A], y=X_test_out.loc[:, chem_B],
+        hue=y_test_names, alpha=0.7,
+        palette=colores(y_test_names, target_type), ax=axes[0]
+    )
+    axes[0].set_title("Original data")
+    axes[0].legend(loc='center left', bbox_to_anchor=(0, -0.65), ncol=2)
+
+    # Plot Imputed data with ground truth labels
+    sns.scatterplot(
+        x=X_test_imp.loc[:, chem_A], y=X_test_imp.loc[:, chem_B],
+        hue=y_test_names, alpha=0.7,
+        palette=colores(y_test_names, target_type), ax=axes[1])
+    sns.scatterplot(
+        x=X_test_imp.loc[ind_wrong, chem_A],
+        y=X_test_imp.loc[ind_wrong, chem_B],
+        ax=axes[1], marker='x', color='k', s=30
+    )
+    axes[1].set_title(
+        "Imputed and normalized test data \n with ground truth labels")
+    axes[1].legend(loc='center left', bbox_to_anchor=(0, -0.65), ncol=2)
+
+    # Plot Imputed data with predicted labels
+    y_pred_names = volcano_list[pred]
+    sns.scatterplot(
+        x=X_test_imp.loc[:, chem_A],  y=X_test_imp.loc[:, chem_B],
+        hue=y_pred_names, alpha=0.7,
+        palette=colores(y_pred_names, target_type), ax=axes[2]
+    )
+    sns.scatterplot(
+        x=X_test_imp.loc[ind_wrong, chem_A],
+        y=X_test_imp.loc[ind_wrong, chem_B],
+        ax=axes[2], marker='x', color='k', s=30
+    )
+    axes[2].set_title(
+        "Imputed and normalized test data \n with predicted labels")
+    axes[2].legend(loc='center left', bbox_to_anchor=(0, -0.65), ncol=2)
+
+    if save:
+        plt.savefig(
+            '../figures/' + name + '_' + chem_A + 'vs' + chem_B + '.png',
+            dpi=300, bbox_inches='tight', facecolor='w')
+    else:
+        plt.show()
+
+
+def plot_confusion_matrix(y_test_names, y_pred_names, labels, name='default',
+                          save=False):
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111)
+    cm = confusion_matrix(
+        y_test_names, y_pred_names, labels=labels, normalize='true'
+    )
+    cm = (cm.T/cm.sum(axis=1)).T
+    plt.imshow(cm, cmap='viridis')
+    plt.colorbar()
+    n_volcanoes = len(labels)
+    ax.set_xticks(np.arange(n_volcanoes))
+    ax.set_xticklabels(labels)
+    ax.set_yticks(np.arange(n_volcanoes))
+    ax.set_yticklabels(labels)
+    plt.ylabel('True label', fontsize=14)
+    plt.xlabel('Predicted label', fontsize=14)
+    plt.xticks(rotation=90)
+    if save:
+        plt.savefig('../figures/'+name+'.png', dpi=300, bbox_inches='tight',
+                    facecolor='w')
+    else:
+        plt.show()
+
+
+def get_design_matrix(df_volcanoes, data_type):
+    # Retrieve the geochemical data. FeO, Fe2O3 and FeO2O3T are dropped because
+    # FeOT is a different expression of the same element (Fe).
+    # P2O5 and Cl are also dropped because they are sporadically analyzed.
+    majors = ['SiO2_normalized', 'TiO2_normalized', 'Al2O3_normalized',
+              'FeOT_normalized',
+              # 'FeO_normalized', 'Fe2O3_normalized', 'Fe2O3T_normalized',
+              'MnO_normalized', 'MgO_normalized', 'CaO_normalized',
+              'Na2O_normalized', 'K2O_normalized',
+              # 'P2O5_normalized','Cl_normalized'
+              ]
+
+    traces = ['Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Cs', 'Ba', 'La',
+              'Ce', 'Pr', 'Nd', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy',
+              'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'Pb',
+              'Th', 'U']
+
+    if data_type == 'majors_and_traces':
+        X_major = df_volcanoes.loc[:, majors]
+        X_traces = df_volcanoes.loc[:, traces]
+        X_volcanoes = pd.concat([X_major, X_traces], axis=1)
+
+    elif data_type == 'majors_only':
+        X_major = df_volcanoes.loc[:, majors]
+        # Remove rows with only missing values
+        mask_major = X_major.isna()
+        ind_not_all_na = np.where(
+            mask_major.sum(axis=1) != X_major.shape[1])[0]
+        X_volcanoes = X_major.iloc[ind_not_all_na, ]
+        df_volcanoes = df_volcanoes.iloc[ind_not_all_na, ]
+
+    elif data_type == 'traces_only':
+        X_traces = df_volcanoes.loc[:, traces]
+        # Remove rows with only missing values
+        mask_traces = X_traces.isna()
+        ind_not_all_na = np.where(
+            mask_traces.sum(axis=1) != X_traces.shape[1])[0]
+        X_volcanoes = X_traces.iloc[ind_not_all_na, ]
+        df_volcanoes = df_volcanoes.iloc[ind_not_all_na, ]
+
+    else:
+        raise ValueError(f'`data_type` is {data_type}. Should be one of\
+             majors_and_traces, majors_only or  traces_only')
+
+    return df_volcanoes, X_volcanoes
+
+
+def get_models():
+
+    # K Nearest Neighbors
+    knn = KNeighborsClassifier()
+    grid_knn = {'kneighborsclassifier__n_neighbors': [2, 5, 10],
+                'kneighborsclassifier__weights': ['uniform', 'distance']}
+
+    # Multinomial logistic regression
+    lr = LogisticRegression(multi_class='multinomial', class_weight='balanced')
+    grid_lr = {'logisticregression__C': [1e-2, 1e-1, 1, 1e1, 1e2],
+               'logisticregression__solver': ['saga']}
+
+    # Random Forest
+    # usar bootstrap false o class weight balanced_subsample no tiene gran
+    # efecto!
+    rf = RandomForestClassifier(class_weight='balanced', bootstrap=False,
+                                verbose=0)
+    grid_rf = {'randomforestclassifier__n_estimators': [50, 100, 200],
+               'randomforestclassifier__min_samples_leaf': [1, 5]}
+
+    # Gradient Boosting
+    gb = HistGradientBoostingClassifier()
+    grid_gb = {
+        'histgradientboostingclassifier__learning_rate':
+            [1e-4, 1e-3, 1e-2, 1e-1],
+        'histgradientboostingclassifier__l2_regularization': [0, 1e-2, 1e-1],
+        'histgradientboostingclassifier__max_depth': [None, 3]
+        }
+
+    # Define imputation models
+    # ------------------------
+    # Simple Imputer
+    mean_imp = SimpleImputer(strategy='mean')
+
+    # KNN Imputer
+    knn_imp = KNNImputer(n_neighbors=15, weights='distance')
+
+    # Iterative Imputer with Bayesian Ridge
+    br_imp = IterativeImputer(estimator=BayesianRidge(), min_value=0)
+
+    # Iterative Imputer with Random Forest
+    est_rf = RandomForestRegressor()
+    rf_imp = IterativeImputer(estimator=est_rf, min_value=0)
+
+    # Define the imputation-model combinations to be tested
+    # -----------------------------------------------------
+    models = {'knn': knn, 'lr': lr, 'rf': rf, 'gb': gb}
+    imputations = {'knn_imp': knn_imp, 'br_imp': br_imp, 'mean_imp': mean_imp,
+                   'rf_imp': rf_imp}
+    grids = {'knn': grid_knn, 'lr': grid_lr, 'rf': grid_rf, 'gb': grid_gb}
+
+    return imputations, models, grids
+
+
+def simbologia(volcano, event):
+
+    simbología = pd.read_csv('../../Scripts/Simbologia.csv',
+                             encoding='latin1', low_memory=False)
+    Event = simbología.loc[simbología['Volcano'] == volcano]
+    Event = Event.loc[Event['Event'] == event]
+    coloR = Event.values[0, 2]
+    markeR = Event.values[0, 3]
+    return coloR, markeR
+
+
+def colores(Y, type):
+    Dpal = {}
+    if type == 'volcano':
+        for i, volcan in enumerate(np.unique(Y)):
+            color, marker = simbologia(volcan, 'Unknown')
+            Dpal[volcan] = color
+
+    if type == 'event':
+        simbología = pd.read_csv('../../Scripts/Simbologia.csv',
+                                 encoding='latin1', low_memory=False)
+        for event in np.unique(Y):
+            # print(event)
+            color, marker = simbologia(
+                simbología[simbología.Event == event].Volcano.values[0], event)
+            Dpal[event] = color
+
+    return Dpal
